76bf1ac846516e400b6e68859d8caa8cc67c2265
alphacentaury-github <44492142+alphacentaury-github@users.noreply.github.com>
2024-02-01 18:02:17 +0900

    update

--- EigenVectorContinuation.py
+++ EigenVectorContinuation.py
@@ -5,15 +5,98 @@ Created on Fri Jan 26 16:06:40 2024
 @author: young
 """
 #------------------EC-----------------------------------------------------------
+import numpy as np
+import scipy 
+import torch 
+import torch.nn as nn
+from torch.autograd import Variable
+
+#==============================================================================
 class EigenVectorContinuation():
-    def __init__(self,H_constructor,train_c,num_lowest_lvl=1):
-        self.H_constructor = H_constructor 
-        self.train_c = train_c 
-        self.num_lowest_lvl = num_lowest_lvl 
-        self.train_vectors =None         
-    def make_train_data(self,):
+    """
+    H_constructor : a function which returns H(c) matrix 
+    train_c       : list of training points 
+    level_index   : n-th level to eigen-vector continuation 
+    
+                    
+    For example:
+        
+    ec_a = EigenVectorContinuation(get_Htot,[0.5,1.5],0)
+    ec_a.prepare()
+    ec_a.predict(1.0)                 
+          
+    works to do:  
+        (1) use level_index list 
+        (2) use list of c for prediction                 
+    """
+    def __init__(self,H_constructor,train_c,level_index=0):
+        self.H_constructor = H_constructor # H(c) matrix constructor 
+        self.train_c = np.array(train_c) # training input parameters 
+        self.dim_ec = len(train_c) 
+        self.train_vectors =None
+        self.level_index = 0 # level to use for EC          
+        
+    def prepare(self,):
+        eigvals = np.zeros_like(self.train_c)
+        eigvecs = [ ] 
+        for i,c in enumerate(self.train_c):
+            Htot = self.H_constructor(c)
+            ee,vv = scipy.linalg.eigh(Htot) #sorted eigenvalue and eigenvector   
+            eigvals[i] = ee[self.level_index] # n-th eigenvalue
+            eigvecs.append( vv[:,self.level_index] ) # n-th eigenvector 
+        eigvecs = np.array(eigvecs)     
+        self.train_vectors = eigvecs 
+        return  
+    
+    def get_reduced_Hamiltonian(self,c):
+        #--EC Hamiltonian and Norm matrix
+        Htot = self.H_constructor(c) 
+        H_EC = np.zeros( (self.dim_ec,self.dim_ec) )
+        N_EC = np.zeros( (self.dim_ec,self.dim_ec) )
+        for i in range(self.dim_ec):
+            vL = self.train_vectors[i]
+            for j in range(self.dim_ec):
+                vR = self.train_vectors[j]
+                H_EC[i,j] = np.matmul(vL, np.matmul(Htot,vR))
+                N_EC[i,j] = np.matmul(vL, vR)
+        return H_EC, N_EC 
+    
+    def predict(self,c):
+        H_EC, N_EC = self.get_reduced_Hamiltonian(c)
+        ee, vv = scipy.linalg.eigh(H_EC,N_EC)
+        return ee[self.level_index], vv[:,self.level_index]
+
+#============================================================================== 
+def random_Hermitian_variable(ndim):   
+    #----construct Hermitian matrix from input ndim**2 variable 
+    var = Variable(torch.randn(ndim),requires_grad=True)
+    var2 = Variable(torch.randn(int(ndim*(ndim-1)/2),dtype=torch.complex64),requires_grad=True)
+    mm = torch.zeros((ndim,ndim))
+    indices = np.diag_indices(ndim)
+    mm[indices] = var 
+    mm = mm.type(torch.complex64)
+    indices = np.triu_indices(ndim,1) # indices of upper traiangle without diagonal 
+    mm[indices] = var2
+    mm = mm + torch.transpose(mm.conj(),0,1) 
+    return mm  , (var,var2)
+   
+class ParametricMatrixModel():
+    """
+      ndim : dimension of PMM 
+      train_data : set of c_list, eigenvalues 
+               
+      1. create Variables for PMM 
+      2. create Hermitian Matrix 
+    """
+    def __init__(self,ndim,ansatz_constructor,n_mats,optimizer=None,loss_type=None):
+        self.ansatz = ansatz_constructor  
+        self.optim = None 
+        self.get_loss = None 
+        self.n_mats = n_mats # number of Hermitian matrices for ansatz 
         return 
-    def get_reduced_Hamiltonian(self,):
+    def prepare(self,):
         return 
-    def predict(self,):
+    def train(self,train_data,epochs=5000):
+        return 
+    def predict(self,c):
         return 
--- lattice_1d.py
+++ lattice_1d.py
@@ -12,6 +12,8 @@ from torch.autograd import Variable
 import scipy
 import matplotlib.pyplot as plt
 
+from EigenVectorContinuation import * 
+
 #-----------setup lattice----------------------------------
 hbarc = 197.3
 L = 40
@@ -57,7 +59,7 @@ def get_HKin(iKin,test=False):
         # However, in other dimension, one have to use summation. 
     
     if test:
-        ee,vv = scipy.linalg.eig(Hfree)
+        ee,vv = scipy.linalg.eigh(Hfree)
         print('p^2  Hkin:{} '.format(np.sort(ee)))
         if iKin==0: print('p^2  exa :{} '.format(np.sort(qx**2) )) 
         if iKin>0:
@@ -67,125 +69,108 @@ def get_HKin(iKin,test=False):
 
 ## Test calculation with the answer 
 
+def WS_pot(x,V0,R0,a=1.0):
+    # evrything  in l.u.
+    # x must be a distance, not coordinate  
+    return V0/(1.+np.exp((abs(x)-R0)/a))
+def sqwell_pot(x,V0,R0):
+    # evrything  in l.u.
+    # x must be a distance, not coordinate  
+    return V0*(abs(x) <= R0)
+
 #----set potential
-R0 = 3.0/a_fm # 2 fm -> l.u 
-#V0 = -10.0/cutoff # 10 MeV -> l.u 
-V0 = -20./mass/R0**2
-print('V0={} MeV, R0={} fm'.format(V0*cutoff,R0*a_fm))
+def test_case_1d():
+    R0 = 3.0/a_fm # 2 fm -> l.u 
+    #V0 = -10.0/cutoff # 10 MeV -> l.u 
+    V0 = -20./mass/R0**2
+    print('V0={} MeV, R0={} fm'.format(V0*cutoff,R0*a_fm))
 
-#---Wiki sol-------
-vv = np.array([1.28, 2.54, 3.73])
-print('E_continuum={} MeV'.format( (2*vv**2/mass/(2*R0)**2 +V0)*cutoff))
+    #---Wiki sol-------
+    vv = np.array([1.28, 2.54, 3.73])
+    print('E_continuum={} MeV'.format( (2*vv**2/mass/(2*R0)**2 +V0)*cutoff))
 
-#---solve S.E. 
-Hfree = get_HKin(iKin)
-H_V = np.zeros((L,L))
-H_V[ abs(dx)< R0,abs(dx)< R0 ] = V0
-Htot = Hfree+H_V
+    #---solve S.E. 
+    Hfree = get_HKin(iKin)
+    H_V = np.zeros((L,L))
+    #H_V[ abs(dx)< R0,abs(dx)< R0 ] = V0    
+    VV = sqwell_pot(dx,V0,R0)
+    H_V[r,r] = VV 
+    
+    Htot = Hfree+H_V
 
-#----get first 20 eigenvalues
-#ee = scipy.linalg.eigvals(Htot)
-#print(np.sort(ee)[:20]*cutoff)
-#--or use torch
-print('E_lattice={} MeV'.format(torch.linalg.eigvalsh(torch.Tensor(Htot))[:4]*cutoff))
-
-## Test PMM with square-well potential problem 
+    #----get first 20 eigenvalues
+    #ee = scipy.linalg.eigvals(Htot)
+    #print(np.sort(ee)[:20]*cutoff)
+    #--or use torch
+    print('E_lattice={} MeV'.format(scipy.linalg.eigvalsh(Htot)[:4]*cutoff))
+    #print('E_lattice={} MeV'.format(torch.linalg.eigvalsh(torch.Tensor(Htot))[:4]*cutoff))
+    return 
 
 #---prepare data
 #clist = [0.5,0.6,0.7,0.8,0.9,1.5]
 #clist = [0.1,0.3,0.5,0.8]
-clist = [0.5,0.55,0.6,0.65,0.7,0.75,0.8,0.85,0.9,1.5]
-Hfree = get_HKin(iKin)
+clist = [0.5,0.52,0.55,0.6,0.62,0.65,0.7,0.72,0.75,0.8,0.82,0.85,0.9,1.5] # a training points for PMM 
+c_p = np.arange(0.1,2.5,0.1) # full test range 
+
+#---construct Hamiltonian 
 R0 = 3.0/a_fm # fm -> l.u 
 V0 = -100.0/cutoff # MeV -> l.u 
+a0 = 1.0 # in l.u. 
 
-def WS_pot(x,V0,R0,a):
-    return V0/(1.+np.exp((abs(x)-R0)/a))
+Hfree = get_HKin(iKin) 
 
-
- 
-data=[]
-for c in clist:
+def get_Htot(c):
     H_V = np.zeros((L,L))
-    #H_V[ abs(dx)< R0,abs(dx)< R0 ] = c*V0    
-    VV = WS_pot(dx,V0,c*R0,1.0)
+    VV = WS_pot(dx,V0,c*R0,a0)
     H_V[r,r] = VV 
     Htot = Hfree+H_V
+    return Htot 
+
+#------training data-------------------------------------------------- 
+data=[]
+for c in clist:
+    Htot = get_Htot(c) 
     data.append(torch.linalg.eigvalsh(torch.Tensor(Htot)).numpy()[:2])
 data = np.array(data)
-print(data)    
+ 
 data_tensor = torch.tensor(data)
 
+#-----eigenvector continuation----------------------------------------
 ## Eigenvector Continuation test
 #--prepare EC training data 
+
+clist_ec = [0.5,1.5]
+
+ec_test0 = EigenVectorContinuation(get_Htot,clist_ec,0)
+ec_test0.prepare()  
+ec_test1 = EigenVectorContinuation(get_Htot,clist_ec,1)
+ec_test1.prepare()
 ec_evals0=[]
 ec_evals1=[]
 ec_vects0=[]
 ec_vects1=[]
-ec_mats =[]
-for c in [0.5,1.5]:
-    H_V = np.zeros((L,L))
-    #H_V[ abs(dx)< R0,abs(dx)< R0 ] = c*V0
-    VV = WS_pot(dx,V0,c*R0,1.0)
-    H_V[r,r] = VV 
-    
-    Htot = Hfree+H_V
-    #ee,vv = np.linalg.eigh(Htot)
-    ee,vv = scipy.linalg.eigh(Htot)
-    ec_mats.append(Htot)
-    ec_evals0.append(ee[0]) # gs(?) it is not clear whether the ordering is correct...
-    ec_vects0.append(vv[:,0])
-    ec_evals1.append(ee[1]) # gs(?) it is not clear whether the ordering is correct...
-    ec_vects1.append(vv[:,1])
-    # print('test eigen:{} <H>:{}'.format(ee[0], vv[:,0]@ Htot@vv[:,0]  ) )
-ec_mats = np.array(ec_mats)
+ec_Htot =[]
+
+for c in clist_ec:
+    Htot = get_Htot(c) 
+    ee,vv = ec_test0.predict(c)
+    ec_evals0.append(ee)
+    ec_vects0.append(vv)
+    ee,vv = ec_test1.predict(c)
+    ec_evals1.append(ee)
+    ec_vects1.append(vv)
+
 ec_evals0 = np.array(ec_evals0)
-ec_vects0 = np.array(ec_vects0)
+ec_vects0 = np.array(ec_vects0) # gs eigen vectors at c1,c2 
 ec_evals1 = np.array(ec_evals1)
-ec_vects1 = np.array(ec_vects1)
-#----consistency test of eigenvector, <H>
-for i in range(2):
-    print('{}: eigv={} <H>={}'.format(i,ec_evals0[i], ec_vects0[i,:]@ ec_mats[i] @ ec_vects0[i,:]))
-
-def EC_test0(c):
-    #----prediction by EC
-    H_V = np.zeros((L,L))
-    #H_V[ abs(dx)< R0,abs(dx)< R0 ] = c*V0
-    VV = WS_pot(dx,V0,c*R0,1.0)
-    H_V[r,r] = VV 
-    Htot = torch.Tensor(Hfree+H_V) # H(c) 
-    #---construct reduced matrix and norm matrix 
-    Mc = np.zeros((2,2))
-    Nc = np.zeros((2,2))
-    for i in range(2):
-        for j in range(2):
-            Mc[i,j] = np.matmul(ec_vects0[i,:],np.matmul(Htot,ec_vects0[j,:]))
-            Nc[i,j] = np.matmul(ec_vects0[i,:],ec_vects0[j,:])
-    ee,vv = scipy.linalg.eig(Mc,Nc) 
-    return ee, vv
-
-def EC_test1(c):
-    #----prediction by EC
-    H_V = np.zeros((L,L))
-    #H_V[ abs(dx)< R0,abs(dx)< R0 ] = c*V0
-    VV = WS_pot(dx,V0,c*R0,1.0)
-    H_V[r,r] = VV 
-    Htot = torch.Tensor(Hfree+H_V) # H(c) 
-    #---construct reduced matrix and norm matrix 
-    Mc = np.zeros((2,2))
-    Nc = np.zeros((2,2))
-    for i in range(2):
-        for j in range(2):
-            Mc[i,j] = np.matmul(ec_vects1[i,:],np.matmul(Htot,ec_vects1[j,:]))
-            Nc[i,j] = np.matmul(ec_vects1[i,:],ec_vects1[j,:])
-    ee,vv = scipy.linalg.eig(Mc,Nc) 
-    return ee, vv
-
+ec_vects1 = np.array(ec_vects1) # ex eigen vectors at c1,c2 
+#-------------------------------------------------------------------------------------------
+# test of ground and excited states together 
 def EC_both(c):
     #----prediction by EC
     H_V = np.zeros((L,L))
     #H_V[ abs(dx)< R0,abs(dx)< R0 ] = c*V0
-    VV = WS_pot(dx,V0,c*R0,1.0)
+    VV = WS_pot(dx,V0,c*R0,a0)
     H_V[r,r] = VV 
     Htot = torch.Tensor(Hfree+H_V) # H(c) 
     #---construct reduced matrix and norm matrix 
@@ -203,39 +188,15 @@ def EC_both(c):
                 vR = ec_vects1[j-2,:]
             Mc[i,j] = np.matmul(vL,np.matmul(Htot,vR))
             Nc[i,j] = np.matmul(vL,vR)
-    ee,vv = scipy.linalg.eig(Mc,Nc) 
+    ee,vv = scipy.linalg.eigh(Mc,Nc) 
     return ee, vv, Mc, Nc 
     
-def EC_test(c):
-    #----prediction by EC
-    H_V = np.zeros((L,L))
-    #H_V[ abs(dx)< R0,abs(dx)< R0 ] = c*V0
-    VV = WS_pot(dx,V0,c*R0,1.0)
-    H_V[r,r] = VV 
-    Htot = torch.Tensor(Hfree+H_V) # H(c) 
-    #---construct reduced matrix and norm matrix 
-    Mc = np.zeros((2,2))
-    Nc = np.zeros((2,2))
-    for i in range(2):
-        if i ==0 : 
-            vL = ec_vects0[i,:]
-        else:
-            vL = ec_vects1[i,:]
-        for j in range(2):
-            if j==0:
-                vR = ec_vects0[j,:]
-            else:
-                vR = ec_vects1[j,:]
-            Mc[i,j] = np.matmul(vL,np.matmul(Htot,vR))
-            Nc[i,j] = np.matmul(vL,vR)
-    ee,vv = scipy.linalg.eig(Mc,Nc) 
-    return ee,vv,Mc,Nc
-
-ee,vv=EC_test0(1.5)
-print('EC e={}'.format(ee[1]))
-
-### 2d PMM
-
+### 2d PMM================================================================
+# This 2d PMM is trained for gs and 1st excited states
+# And Hermitian Matrices are prepared by using Pauli matrices...
+# However, that might be a problem. 
+# One may need to increase the dimension of PMM 
+# or use separate PMM for g.s. and excited state. 
 s0 = torch.tensor( [ [1.0,0.0],[0.0,1.0]])
 sx = torch.tensor( [ [0.0,1.0],[1.0,0.0]])
 sy = torch.tensor( [ [0.0+0j,-1j],[1j,0.0+0j]],dtype=torch.complex64)
@@ -244,25 +205,29 @@ sz = torch.tensor( [ [1.0,0.0],[0.0,-1.0]])
 var1 = Variable(torch.tensor([0.5,0.5,0.5,0.5]),requires_grad=True)
 var2 = Variable(torch.tensor([0.5,0.5,0.5,0.5]),requires_grad=True)
 var3 = Variable(torch.tensor([0.5,0.5,0.5,0.5]),requires_grad=True)
-# optim = torch.optim.Adam([var1,var2],lr=2.e-3)
-optim = torch.optim.Adam([var1,var2,var3],lr=2.e-3)
-#optim = torch.optim.SGD([var1,var2])
+var4 = Variable(torch.tensor([0.5,0.5,0.5,0.5]),requires_grad=True)
+var5 = Variable(torch.tensor([0.5,0.5,0.5,0.5]),requires_grad=True)
+
+optim = torch.optim.Adam([var1,var2],lr=2.e-3)
 get_loss = nn.MSELoss(reduction='sum')
 
 def mm_var(var):
     return s0*var[0]+sx*var[1]+sy*var[2]+sz*var[3]
-
-for ii in range(6000):
-  dd = torch.zeros_like(data_tensor)
-  for i,c in enumerate(clist):
-      M1 = mm_var(var1)
-      M2 = mm_var(var2)
-      M3 = mm_var(var3)
-      #Mtot = M1 + c *M2
-      Mtot = M1 + c *M2 +c**2*M3
-      dd[i] = torch.linalg.eigvalsh(Mtot)[:2]
     
-  loss = get_loss(dd,data_tensor)
+def get_Mtot(c): # ansatz of PMM 
+    M1 = mm_var(var1)
+    M2 = mm_var(var2)
+    Mtot = M1 + M2/c 
+    return Mtot 
+
+#=====repeat as many to reduce the loss 
+for ii in range(6000):
+  dd = torch.zeros_like(data_tensor[:,0]) # use data_tensor to use both energies 
+  for i,c in enumerate(clist):
+      Mtot = get_Mtot(c)
+      dd[i] = torch.linalg.eigvalsh(Mtot)[:1] # [:2] to use both energies  
+    
+  loss = get_loss(dd,data_tensor[:,0]) # use data_tensor to use both energies 
   optim.zero_grad()
   loss.backward()
   optim.step()
@@ -273,37 +238,55 @@ out_t=[]
 out_p=[]
 out_ec0=[]
 out_ec1=[]
-c_p = np.arange(0.1,2.5,0.1)
+
+ec_mats=[] 
 for c in c_p:
   H_V = np.zeros((L,L))
   #H_V[ abs(dx)< R0,abs(dx)< R0 ] = c*V0
-  VV = WS_pot(dx,V0,c*R0,1.0)
+  VV = WS_pot(dx,V0,c*R0,a0)
   H_V[r,r] = VV 
   Htot = Hfree + H_V
   out_t.append(torch.linalg.eigvalsh(torch.Tensor(Htot)).numpy()[:2])
     
   with torch.inference_mode():
-    M1 = mm_var(var1)
-    M2 = mm_var(var2)
-    M3 = mm_var(var3)
-    Mtot = M1 + c *M2 +c**2*M3    
+    Mtot = get_Mtot(c)
     out_p.append(torch.linalg.eigvalsh(Mtot).numpy())
   #--EC results 
-  ee,vv = EC_test0(c)
-  out_ec0.append(np.real( np.min(ee) ))
-  ee,vv = EC_test1(c)
-  out_ec1.append(np.real( np.min(ee) ))
+  temp1, temp2 = ec_test0.get_reduced_Hamiltonian(c)
+  ee,vv = ec_test0.predict(c)
+  out_ec0.append(np.real(ee))
+  isqnn = scipy.linalg.fractional_matrix_power(temp2,-0.5) #sqrt(N^-1)
+  ec_mats.append( isqnn @ temp1 @ isqnn ) # Hermitian EC Hamiltonian 
+  
+  ee,vv = ec_test1.predict(c)
+  out_ec1.append(ee)
+  
 out_t = np.array(out_t)
 out_p = np.array(out_p)
 out_ec0 = np.array(out_ec0)
 out_ec1 = np.array(out_ec1)
 #---------------------------------------
-out_ec_test=[]
-for c in c_p:
-    ee,vv,Mc,Nc = EC_test(c)
-    ee,vv,Mc,Nc = EC_both(c)
-    out_ec_test.append(np.sort(np.real(ee)))
-out_ec_test= np.array(out_ec_test)    
+#tt =[]
+#for i,c in enumerate(c_p):
+#    tt.append(ec_mats[i][0,0])
+#tt=np.array(tt)
+
+##f = lambda x, a, b,c,d,e : a*x**2+b*x+c+d/x+e/x**2
+#f = lambda x, a, b,c,d,e : a*x**2+b*x+c+d/x
+##f = lambda x, b,c,d : b*x+c+d/x
+##f = lambda x, a, b,c : a*np.exp(-b*x)+c
+#popt,pcov =scipy.optimize.curve_fit(f,c_p,tt)
+#plt.plot(c_p,tt,'.',label='EC')
+#plt.plot(c_p,f(c_p,*popt),label='fit')
+#plt.xlabel('c');plt.ylabel(r'$M_{00}$')
+#plt.legend()     
+#---------------------------------------
+#out_ec_test=[]
+#for c in c_p:
+#    ee,vv,Mc,Nc = EC_test(c)
+#    #ee,vv,Mc,Nc = EC_both(c)
+#    out_ec_test.append(np.sort(np.real(ee)))
+#out_ec_test= np.array(out_ec_test)    
 
 #-----plot------------------------------
 plt.plot(clist,data[:,0],'g*')
@@ -311,11 +294,12 @@ plt.plot(clist,data[:,1],'g*')
 plt.plot(c_p,out_t[:,0],'r--',label='true e0' )
 plt.plot(c_p,out_t[:,1],'b--',label='true e1' )
 plt.plot(c_p,out_p[:,0],'r',label='PMM e0' )
-plt.plot(c_p,out_p[:,1],'b',label='PMM e1' )
+#plt.plot(c_p,out_p[:,1],'b',label='PMM e1' )
 plt.plot(c_p,out_ec0,'y-.',label='EC e0')
 plt.plot(c_p,out_ec1,'y-.',label='EC e1')
-plt.plot(c_p,out_ec_test[:,0],'g-.',label='EC_test e0')
-plt.plot(c_p,out_ec_test[:,1],'g-.',label='EC_test e1')
+#plt.plot(c_p,out_ec_test[:,0],'g-.',label='EC_test e0')
+#plt.plot(c_p,out_ec_test[:,1],'g-.',label='EC_test e1')
 
 plt.xlabel('c');plt.ylabel('E [l.u.]')
+plt.ylim([-0.5,0.1])
 plt.legend()
